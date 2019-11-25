d98afb5f9324c70421eea7753716e7fc515f33c7
Xavi Moreira <moreiraje@vcu.edu>
2019-11-24 19:53:41 -0500

    removed duplicates in choice feild

--- stone_cold_props/property_app/search.py
+++ stone_cold_props/property_app/search.py
@@ -2,13 +2,28 @@ from django import forms
 
 from .models import *
 
+def rem_duplicate_city():
+    w = Address.objects.all()
+    c = ['*']
+    for x in w:
+        if x.city not in c:
+            c.append(x.city)
+    return c
+
+def rem_duplicate_state():
+    w = Address.objects.all()
+    c = ['*']
+    for x in w:
+        if x.state not in c:
+            c.append(x.state)
+    return c
 
 class searchForm(forms.Form):
 
 
-    houseType = forms.ChoiceField(choices=[('*', '-'), ('house', 'House'), ('apartment', 'Apartment')])
-    city = forms.ChoiceField(choices=[(x.building, x.city) for x in Address.objects.all()])
-    state = forms.ChoiceField(choices=[(x.state, x.state) for x in Address.objects.all()])
+    houseType = forms.ChoiceField(choices=[('*', '-'), ('House', 'House'), ('Apartment', 'Apartment')])
+    city = forms.ChoiceField(choices=[(x, x) for x in rem_duplicate_city()])
+    state = forms.ChoiceField(choices=[(x, x) for x in rem_duplicate_state()])
     address = forms.ChoiceField(choices=[(x.street, x.street) for x in Address.objects.all()])  #this one shouldnt be here but is just for tests
 
     #TODO get duplates out of the for look mayeb a UNIQUE method
